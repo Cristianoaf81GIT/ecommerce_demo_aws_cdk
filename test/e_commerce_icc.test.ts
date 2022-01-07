e26a32370908ce1d0e2ceb8d26291e2d1c7004de
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-07 09:36:57 -0300

    Initial commit

---
+++ test/e_commerce_icc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ECommerceIcc from '../lib/e_commerce_icc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/e_commerce_icc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ECommerceIcc.ECommerceIccStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
